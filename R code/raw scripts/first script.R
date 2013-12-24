7aec1e40fd7ca58d5f5e12ac3b4de1b8848b5e13
IamGianluca <gr.gianlucarossi@gmail.com>
2013-12-24 23:00:32 +0000

    Started exploratory analysis on variable Age

--- R code/raw scripts/first script.R
+++ R code/raw scripts/first script.R
@@ -1,14 +1,35 @@
-#set working directory
-setwd("~/Dropbox//Data Analysis/titanic/")
+library(ggplot2)
 
-# load datasets
-gender <- read.csv("./data//raw data/gendermodel.csv", stringsAsFactors=F)
-train.set <-read.csv("./data//raw data/train.csv", stringsAsFactors=F)
+## set working directory
+setwd("~/Dropbox/Data Analysis/titanic/")
 
-# set some variables as factors..
+## load datasets
+data <-read.csv("./data/raw data/train.csv")
 
-# exploratory analysis
-head(gender)
-head(train.set)
-str(train.set)
-summary(train.set)
+str(data)
+
+## data munging
+data$Survived <- as.factor(data$Survived)
+data$Pclass <- as.factor(data$Pclass)
+data$Name <- as.character(data$Name)
+data$Ticket <- as.integer(data$Ticket)
+
+## exploratory analysis
+## variable "Age"
+par(mfrow = c(1, 2))
+hist(data$Age, breaks=100)
+plot(density(data$Age, na.rm=T))
+
+# interestingly althogh the median is similar on both groups (survived and died) the
+# distributions are skewed towards different sides! 
+qplot(Survived, Age, geom="boxplot", data=data)
+
+
+
+###########
+
+## variable "Ticket"
+# maybe trivial, because the ticket number doesn't seem to be a relevant covariate, however
+# apparently there are tickets which have been issued for more than one person (group
+# tickets for families, maybe? please investigate)
+hist(table(data$Ticket))
