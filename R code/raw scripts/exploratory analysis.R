ae08a42424b8f9fb713341b243ea982c2cf6ef15
IamGianluca <gr.gianlucarossi@gmail.com>
2013-12-26 13:22:50 +0000

    Continue exploratory analysis, including Age, Pclass, Sex and Fare

--- R code/raw scripts/exploratory analysis.R
+++ R code/raw scripts/exploratory analysis.R
@@ -1,9 +1,14 @@
+##### LOAD RELEVANT LIBRARIES #####
+
 library(ggplot2)
 
-## set working directory
+
+##### PREPARE DATASET #####
+
+# set working directory
 setwd("~/Dropbox/Data Analysis/titanic/")
 
-## load datasets and initial data munging
+# load datasets and initial data munging
 data <-read.csv("./data/raw data/train.csv")
 data$Survived <- as.factor(data$Survived)
 data$Pclass <- as.factor(data$Pclass)
@@ -11,8 +16,9 @@ data$Name <- as.character(data$Name)
 data$Ticket <- as.integer(data$Ticket)
 
 
-## EXPLORATORY ANALYSIS
-## variable 'Age'
+##### EXPLORATORY ANALYSIS #####
+
+## VARIABLE 'AGE'
 # it doesn't have a normal distribution, and has 177 NAs
 summary(data$Age)
 
@@ -21,22 +27,59 @@ hist(data$Age, breaks=100)
 plot(density(data$Age, na.rm=T))
 
 # filtering by 'Survived' we can see both groups have approximately the same median, but 
-# the shape is different. 'Non survived' is right skewed, whereas 'Survived' is
-# left skewed.
+# the shape is different. 'Non survived' is right skewed, whereas 'Survived' is left skewed.
 qplot(Survived, Age, geom="boxplot", data=data)
 
 
-## variable 'Pclass'
+## VARIABLE 'PCLASS'
 # 3rd class represents more than half of the total population (55.11%)
 summary(data$Pclass)
 ggplot(data, aes(x=Pclass)) + geom_bar()
+
+# survival rate is much higher on 1st class passenger and decrease consistently among each
+# class. Surely this is an important covariate!
+prop.table(table(data$Pclass, data$Survived), 1)
+qplot(factor(Pclass), data=data, geom="bar", fill=factor(Survived)) 
 
 # interestingly 1st class population is considerably older than the rest. This might be a
 # potential confounder!
 qplot(Pclass, Age, geom="boxplot", data=data)
 
+# fares on lower classes are cheaper than those on 1st class. That's expectable, although
+# there are some odd cases where 3rd and 2nd class tickets were more expensive than the 
+# average 1st class ticket
+qplot(Pclass, Fare, geom="boxplot", data=data)
 
-## variable 'Ticket'
+
+## VARIABLE 'SEX'
+# males are more numerous than females
+summary(data$Sex)
+qplot(factor(Sex), data=data, geom="bar", fill=factor(Sex))
+
+# females look on average yourger than males
+qplot(Sex, Age, geom="boxplot", data=data)
+
+# famales have a much higher likelihood to survive (.74 vs .19)
+sex_surv <- table(data$Sex, data$Survived)
+prop.table(sex_surv, 1)
+qplot(factor(Sex), data=data, geom="bar", fill=factor(Survived))
+
+
+## VARIABLE 'SIBSP'
+## VARIABLE 'PARCH'
+
+
+## VARIABLE 'FARE'
+# seems that passengers who paid a higher fare had a higher chance to survive
+qplot(Age, Fare, colour=Survived, data=data)
+qplot(Survived, Fare, geom="boxplot", data=data)
+
+
+## VARIABLE 'CABIN'
+## VARIABLE 'EMBARKED'
+
+
+## VARIABLE 'TICKET'
 # maybe trivial, because the ticket number doesn't seem to be a relevant covariate, however
 # apparently there are tickets which have been issued for more than one person (group
 # tickets for families, maybe? please investigate)
