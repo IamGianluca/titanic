65ec251c09ad82776b2baabd1fa51beeab9c691b
IamGianluca <gr.gianlucarossi@gmail.com>
2014-03-03 23:53:49 +0000

    Drop previous part of the code which was using a combined dataset to extract the average age for every title

--- code/data manipulation.R
+++ code/data manipulation.R
@@ -1,7 +1,7 @@
 ##### LOAD RELEVANT LIBRARIES #####
 
 library(dplyr)
-library(ggplot2)
+library(sqldf)
 
 
 options(decimal = 2)
@@ -9,37 +9,42 @@ options(decimal = 2)
 
 ##### LOAD DATASET #####
 
+# set you working directory and load datasets
 setwd("/home/gianluca/Dropbox/Data Analysis/titanic/")
 
-train <- read.csv("./data/train.csv")
-test <- read.csv("./data/test.csv")
+train <- read.csv("./data/train.csv", stringsAsFactors = FALSE)
+test <- read.csv("./data/test.csv", stringsAsFactors = FALSE)
 
 
 ##### DATA MANIPULATION #####
 
-# create Survived column in test dataset and merge the two dataset
-test$Survived <- NA
-combi <- rbind(train, test)
-
-# trasform Name column as character to make text manipulation possible
-combi$Name <- as.character(combi$Name)
-
 # extract the form of address from the Name column
-combi$Title <- gsub(".*, ([^.]*)\\..*", "\\1", combi$Name)
+train$Title <- gsub(".*, ([^.]*)\\..*", "\\1", train$Name)
+test$Title <- gsub(".*, ([^.]*)\\..*", "\\1", test$Name)
 
 # simplify Title schema
-for (i in 1:nrow(combi)) {
-  if (combi$Title[i] == "Mlle") {combi$Title[i] <- "Miss"}
-  else if (combi$Title[i] == "Mme") {combi$Title[i] <- "Mrs"}
-  else if (combi$Title[i] %in% c("Capt", "Don", "Major", "Sir", "Col", "Rev")) {combi$Title[i] <- "Sir"}
-  else if (combi$Title[i] %in% c("Dona", "Lady", "the Countess", "Jonkheer")) {combi$Title[i] <- "Lady"}
-  else if (combi$Title[i] == "Mlle") {combi$Title[i] <- "Miss"}
-
+for (i in 1:nrow(train)) {
+  if (train$Title[i] == "Mlle") {train$Title[i] <- "Miss"}
+  else if (train$Title[i] == "Mme") {train$Title[i] <- "Mrs"}
+  else if (train$Title[i] %in% c("Capt", "Don", "Major", "Sir", "Col", "Rev")) {train$Title[i] <- "Sir"}
+  else if (train$Title[i] %in% c("Lady", "the Countess", "Jonkheer")) {train$Title[i] <- "Lady"}
+  else if (train$Title[i] == "Mlle") {train$Title[i] <- "Miss"}
 }
 
 # summarise Title and average age
-combi %.%
+av_ages <- train %.%
   group_by(Title) %.%
-  summarise(Age = mean(Age, na.rm = TRUE))
+  summarise(avAge = round(mean(Age, na.rm = TRUE), digits = 0))
 
-# 
+# add avAge column to train dataset
+train <- sqldf("select PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket,
+                Fare, Cabin, Embarked, t.Title, avAge 
+                from train as t
+                inner join av_ages as aa
+                  on aa.Title = t.Title")
+
+train %.%
+  select(PassengerId, Title, Age, avAge) %.%
+  filter(is.na(Age) == TRUE)
+
+# go ahead using avAge on those observations where age is NA..
